94570af0bf5ca43b9d0fb05d27cf6c9a60734417
nikhil0035 <49729497+nikhil0035@users.noreply.github.com>
2023-07-19 00:44:30 +0100

    Data_ingestion_stage01

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from Endtoend_Textsummarizer.pipline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Endtoend_Textsummarizer.logging import logger
 
-logger.info("Custom log check")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
