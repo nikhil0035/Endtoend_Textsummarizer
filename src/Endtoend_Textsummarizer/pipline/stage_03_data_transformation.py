f4b624fda4f1cc988a0fc9d755776ac85e66d9d4
nikhil0035 <49729497+nikhil0035@users.noreply.github.com>
2023-07-19 01:53:42 +0100

    Stage_3_Data_transformation

---
+++ src/Endtoend_Textsummarizer/pipline/stage_03_data_transformation.py
@@ -0,0 +1,14 @@
+
+from Endtoend_Textsummarizer.components.data_transformation import DataTransformation
+from Endtoend_Textsummarizer.config.configuration import ConfigurationManager
+from Endtoend_Textsummarizer.logging import logger
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
